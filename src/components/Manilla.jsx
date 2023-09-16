f893189dfa38f2b15e6988fd1bc23e01a11b01ce
JoseDlhB <josehoz.ing@gmail.com>
2023-09-16 16:57:27 -0500

    Consumo API conver dolar tiempo real

--- src/components/Manilla.jsx
+++ src/components/Manilla.jsx
@@ -12,6 +12,7 @@ function Manilla() {
     const [manillas, setManillas] = useState([]);
     const [conversion, setConversion] = useState(1);
     const [moneda, setMoneda] = useState(1);
+    const [valorDolar, setValorDolar] = useState(1);
 
     const addCarrito = async e => {
         e.preventDefault();
@@ -73,7 +74,36 @@ function Manilla() {
             setConversion(1);
 
         } else if (value == 2) {
-            setConversion(5000);
+            const apiUrl = 'http://apilayer.net/api/live?access_key=40da905655b8bfc5c44974226367c4ea&currencies=COP';
+
+            // Crea una nueva instancia de XMLHttpRequest
+            const xhr = new XMLHttpRequest();
+
+            // Configura la solicitud
+            xhr.open('GET', apiUrl, true);
+
+            // Maneja la carga exitosa de datos
+            xhr.onload = function () {
+            if (xhr.status === 200) {
+                const jsonResponse = JSON.parse(xhr.responseText);
+                
+                // Aquí puedes trabajar con los datos recibidos de la API
+                console.log(jsonResponse.quotes.USDCOP.toFixed(2));
+                setValorDolar(jsonResponse.quotes.USDCOP.toFixed(2));
+            } else {
+                console.error('Error en la solicitud a la API');
+            }
+            };
+
+            // Maneja errores de red u otros errores
+            xhr.onerror = function () {
+            console.error('Error en la solicitud a la API');
+            };
+
+            // Envía la solicitud
+            xhr.send();
+            console.log(valorDolar)
+            setConversion(valorDolar);
         }
 
     }
@@ -125,7 +155,7 @@ function Manilla() {
         obtenerTipo();
         obtenerDije();
 
-    })
+    },[])
 
     return (
         <div className='container-fluid'>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import RegistroUsuario from './components/RegistroUsuario'
+import Manilla from './components/Manilla'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RegistroUsuario />
+    <Manilla />
   </React.StrictMode>,
 )
 
