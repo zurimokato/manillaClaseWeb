c6c0b4b27f0c7124822d3f7dae24a46307ab60ed
zurimokato <noj2304@gmail.com>
2023-09-18 18:03:31 -0500

    page not found

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 
 export default function NotFoundPage() {
     return (
-        <div class="wrap-error">
-            <div class="d-flex align-items-center h-100">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-8 offset-sm-2 text-center text-white">
-                            <h1 class=""><span>4</span><span>0</span><span>4</span></h1>
-                            <h5 class="">Oops! Page not found</h5>
-                            <p class="mb-4">we are sorry, but the page you requested was not found</p>
-                            <button class="btn btn-dark" href="#" title="home url">
-                                Go home
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+        <div className="text-center">
+            <h1 className="display-1 fw-bold">404</h1>
+            <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+            <p className="lead">
+                La pagina que estas buscando no existe
+              </p>
+            <a href="/" className="btn btn-primary">Go Home</a>
         </div>
+    </div>
     )
 }
--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,6 +1,9 @@
 import Manilla from '../components/Manilla';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import RegistroUsuario from '../components/RegistroUsuario';
+import NotFoundPage from '../components/NotFoundPage';
+import Home from '../components/Home'
+import Pago from '../components/Pago'
 
 function Rutas() {
     return <Routes>
@@ -9,6 +12,7 @@ function Rutas() {
         <Route path='singIn' element={<RegistroUsuario />} />
         <Route path='manilla' element={<Manilla />} />
         <Route path='pago' element={<Pago />} />
+        <Route path='*' element={<NotFoundPage/>}/>
     </Routes>
 }
 
